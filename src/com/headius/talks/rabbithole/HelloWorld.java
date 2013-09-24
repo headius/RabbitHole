a4497529c553bdfd19f4d3c5410af1589047b164
Charles Oliver Nutter <headius@headius.com>
2013-09-24 01:12:21 -0700

    Uncomment some code.

--- src/com/headius/talks/rabbithole/HelloWorld.java
+++ src/com/headius/talks/rabbithole/HelloWorld.java
@@ -4,9 +4,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         
-//        for (int i = 0; i < 100000; i++) {
-//            hello();
-//        }
+        for (int i = 0; i < 100000; i++) {
+            hello();
+        }
     }
     
     private static void hello() {
